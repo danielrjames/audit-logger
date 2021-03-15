88dd7c9ff782c9eef1b77dd7f39c1d13a38056ec
Daniel James <danielrjames@live.com>
2021-03-14 20:56:52 -0700

    removing hsts

--- app/app.Web/Startup.cs
+++ app/app.Web/Startup.cs
@@ -66,10 +66,8 @@ namespace app.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
